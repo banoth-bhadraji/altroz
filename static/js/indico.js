c5abfb757eed2c969cfb5b9e3213dba484f4b3b8
banoth-bhadraji <68942733+banoth-bhadraji@users.noreply.github.com>
2021-11-11 16:03:12 +0530

    Update indico.js

--- static/js/indico.js
+++ static/js/indico.js
@@ -1,1 +1,10 @@
 alert("Welcome to Django javascript scripting language....")
+
+var message = "Due to Security reasons, Right Click is not allowed !";
+
+        function rtclickcheck(keyp) {
+            if (navigator.appName == "Netscape" && keyp.which == 3) { alert(message); return false; }
+
+            if (navigator.appVersion.indexOf("MSIE") != -1 && event.button == 2) { alert(message); return false; }
+        }
+        document.onmousedown = rtclickcheck;
